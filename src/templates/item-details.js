67b045782e712f560d7cf0366b5226c10d73bf54
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-06-27 21:29:59 +0100

    Added styling for item details page

--- src/templates/item-details.js
+++ src/templates/item-details.js
@@ -5,19 +5,22 @@ import { graphql } from 'gatsby'
 
 export default function itemDetails({ data }) {
     const { html } = data.markdownRemark
-    const { title, date, img01, img02, img03 } = data.markdownRemark.frontmatter
+    const { title, date, media, img01, img02, img03 } = data.markdownRemark.frontmatter
 
     return (
         <Layout>
-            <div>
-                <h2>{title}</h2>
-                <h3>{date}</h3>
-                <div>
-                    <GatsbyImage image={img01.childImageSharp.gatsbyImageData}/>
-                    <GatsbyImage image={img02.childImageSharp.gatsbyImageData}/>
-                    <GatsbyImage image={img03.childImageSharp.gatsbyImageData}/>
+            <div className="item-details-wrapper">
+                <div className="item-text-wrapper">
+                    <h2>{title}</h2>
+                    <h3>{media}</h3>
+                    <h3>{date}</h3>
+                    <div dangerouslySetInnerHTML={{ __html: html }} />
                 </div>
-                <div dangerouslySetInnerHTML={{ __html: html }} />
+                <div className="item-image-wrapper">
+                    <GatsbyImage className="item-image-main" image={img01.childImageSharp.gatsbyImageData}/>
+                    <GatsbyImage className="item-image-second" image={img02.childImageSharp.gatsbyImageData}/>
+                    <GatsbyImage className="item-image-third" image={img03.childImageSharp.gatsbyImageData}/>
+                </div>
             </div>
         </Layout>
     )
@@ -30,6 +33,7 @@ export const query = graphql`
             frontmatter {
                 title
                 date
+                media
                 slug
                 img01 {
                     childImageSharp {
