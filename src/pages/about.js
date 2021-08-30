d9ffa906b6dd5724d5d41e8b3d05d149b806875a
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-08-30 21:31:22 +0100

    Added info to About and Contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,9 @@ export default function about() {
         <Layout>
             <div className="inner-content">
                 <h2>About</h2>
+                <p>Hello <span role="img" aria-label="waving-hand">👋</span> - I'm Candice, the creative hand behind No. 9 Boards.</p>
+                <p>I love creating interesting and unusual designs inspired by old movies, comic art and folk horror.</p>
+                <p>If you would like to purchase one of my cruiser boards, please visit my <a href="https://www.ebay.co.uk/usr/canraffs89">eBay</a> or <a href="https://www.etsy.com/uk/shop/No9Boards">Etsy</a> store.</p>
             </div>
         </Layout>
     )
